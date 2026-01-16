f659047fb2f63980537921fc3f6fbbe31b39ff2f
Avni Singh <avni.dns@gmail.com>
2026-01-16 12:37:02 +0530

    two way binding done

---
+++ 12-two-way-binding/src/App.jsx
@@ -0,0 +1,32 @@
+import react, { useState } from 'react'
+
+const App = () => {
+
+  const [title, settitle] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('form submited', title)
+    settitle('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input 
+          type="text" 
+          placeholder='Enter name'
+          value={title}
+          onChange={(e) => {
+            settitle(e.target.value)
+          }}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
